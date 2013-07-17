29435627fb366c79135b4323c51b28294041865f
Your Name <you@example.com>
2013-07-17 22:39:25 +0000

    Added a search method for submissions

--- src/im/goel/jreddit/submissions/Submissions.java
+++ src/im/goel/jreddit/submissions/Submissions.java
@@ -2,14 +2,16 @@ package im.goel.jreddit.submissions;
 
 import im.goel.jreddit.user.User;
 import im.goel.jreddit.utils.Utils;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.LinkedList;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 
 /**
@@ -71,6 +73,44 @@ public class Submissions {
                     new URL("http://www.reddit.com" + (data.get("permalink").toString()))));
         }
 
+        return submissions;
+    }
+
+    /**
+     *
+     * @param query The search query
+     * @param limit Limit of results(25-100)
+     * @return The list of search results
+     * @throws IOException
+     * @throws ParseException
+     */
+    public static List<Submission> search(String query,int limit) throws IOException, ParseException {
+
+        JSONObject object = (JSONObject) Utils.get("", new URL(
+                "http://www.reddit.com/search.json?q=" + URLEncoder.encode(query, "ISO-8859-1")),
+                null);
+        List<Submission> submissions = null;
+        JSONObject data = (JSONObject) object.get("data");
+        submissions = (List) buildList((JSONArray) data.get("children"), limit);
+        return submissions;
+    }
+    /**
+     * Builds a list of Submissions from the passed array of children.
+     * @author Haukur Rosinkranz
+     */
+    private static List<Object> buildList(JSONArray children, int maxSubmissions) {
+        List<Object> submissions = new ArrayList<Object>(10000);
+        JSONObject obj;
+
+        if (maxSubmissions < 0 || maxSubmissions > children.size()) {
+            maxSubmissions = children.size();
+        }
+        for (int i = 0; i < maxSubmissions; i++) {
+            obj = (JSONObject) children.get(i);
+            JSONObject objd = (JSONObject) obj.get("data");
+            Submission s = new Submission(objd);
+            submissions.add(s);
+            }
         return submissions;
     }
 }
