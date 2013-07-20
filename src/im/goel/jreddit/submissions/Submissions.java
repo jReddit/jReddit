01416481156749cecfae7ca78579a71c759e3e7b
Moe <m.howebrowne@gmail.com>
2013-07-20 17:16:21 +0100

    Sorted out a few of the examples

--- src/im/goel/jreddit/submissions/Submissions.java
+++ src/im/goel/jreddit/submissions/Submissions.java
@@ -21,18 +21,7 @@ import org.json.simple.parser.ParseException;
  * @author <a href="http://www.omrlnr.com">Omer Elnour</a>
  */
 public class Submissions {
-    /**
-     * This function returns a linked list containing the submissions on a given
-     * subreddit and page. (in progress)
-     *
-     * @param redditName The subreddit's name
-     * @param type       HOT or NEW and some others to come
-     * @param frontpage       TODO this
-     * @param user       The user
-     * @return The linked list containing submissions
-     * @throws IOException    If connection fails
-     * @throws ParseException If JSON parsing fails
-     */
+
 	public static LinkedList<Submission> getSubmissions(Subreddit sub, SortOption sort, TimeOption time, int limit, User user) throws IOException, ParseException {
     	
 		LinkedList<Submission> submissions = new LinkedList<Submission>();
@@ -46,6 +35,50 @@ public class Submissions {
 			}
 			try{
 				String urlString = "http://www.reddit.com"+sub.getUrl()+"/"+sort.toString().toLowerCase()+".json?t="+time.toString().toLowerCase()+"&limit=1"+afterString;
+				
+				url = new URL(urlString);
+				Object obj = Utils.get("", url, user.getCookie());
+		        JSONObject object = (JSONObject) obj;
+		        
+		        
+		        JSONObject data = (JSONObject) object.get("data");
+		        after = (String) data.get("after");
+		        JSONArray children = (JSONArray) data.get("children");
+		        
+		        if (!children.isEmpty()){
+		        	JSONObject value = (JSONObject) ((JSONObject) children.get(0)).get("data");
+		        	submissions.add(new Submission(value, user));
+		        	//System.out.println(value.get("title"));
+		        }else{
+		        	j = limit +1;
+		        }
+		        
+	            //submissions.add(new Submission(user, data.get("id").toString(),
+	                    //new URL("http://www.reddit.com" + (data.get("permalink").toString()))));
+
+
+			}catch (Exception e){
+			   e.printStackTrace();
+			}
+			j++;
+		}
+		return submissions;
+    }
+	
+	
+public static LinkedList<Submission> getSubmissions(String sub, SortOption sort, TimeOption time, int limit, User user) throws IOException, ParseException {
+    	
+		LinkedList<Submission> submissions = new LinkedList<Submission>();
+        URL url;
+		String after = "";
+		int j = 0;
+		while (j < limit){
+			String afterString = "";
+			if (j!=0){
+				afterString = "&after="+after;
+			}
+			try{
+				String urlString = "http://www.reddit.com"+sub+"/"+sort.toString().toLowerCase()+".json?t="+time.toString().toLowerCase()+"&limit=1"+afterString;
 				
 				url = new URL(urlString);
 				Object obj = Utils.get("", url, user.getCookie());
--- tests/SubmissionTest.java
+++ tests/SubmissionTest.java
@@ -1,10 +1,9 @@
 import static org.junit.Assert.*;
-
 import im.goel.jreddit.submissions.Submission;
 import im.goel.jreddit.submissions.Submissions;
-import im.goel.jreddit.submissions.Submissions.Page;
-import im.goel.jreddit.submissions.Submissions.Popularity;
 import im.goel.jreddit.user.User;
+import im.goel.jreddit.utils.SortOption;
+import im.goel.jreddit.utils.TimeOption;
 
 import java.util.LinkedList;
 
@@ -14,25 +13,24 @@ import org.junit.Test;
 public class SubmissionTest {
 	@Test
 	public void test() {
-		LinkedList<Submission> frontpage = null;
+		LinkedList<Submission> list = null;
 		Submission first = null;
 
 		try {
 			User user = new User("test_subject_666", "beef");
 			user.connect();
 
-			frontpage = Submissions.getSubmissions("all", Popularity.HOT,
-					Page.FRONTPAGE, user);
-			first = frontpage.get(0);
+			list = Submissions.getSubmissions("all", SortOption.HOT, TimeOption.DAY, 20, user);
+			first = list.get(0);
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
 
 		assertNotSame(
 				"The submission's ID/full name can't be empty, how will reddit identify the submission?",
-				first.fullName, "");
+				first.getTitle(), "");
 		assertNotNull(
 				"The submission's ID/full name can't be null, how will reddit identify the submission?",
-				first.fullName);
+				first.getTitle());
 	}
 }
