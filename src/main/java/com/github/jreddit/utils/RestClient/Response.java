57d9e0279b1174f445febaa0c7244e065af706c8
Tom Beresford <github@tomberesford.co.uk>
2014-03-27 20:49:06 +0000

    renaming pain...

--- src/main/java/com/github/jreddit/utils/RestClient/Response.java
+++
@@ -1,9 +0,0 @@
-package com.github.jreddit.utils.restclient;
-
-public interface Response {
-    public int getStatusCode();
-
-    public Object getResponseObject();
-
-    public String getResponseText();
-}
--- src/main/java/com/github/jreddit/utils/RestClient/RestClient.java
+++
@@ -1,9 +0,0 @@
-package com.github.jreddit.utils.restclient;
-
-public interface RestClient {
-    public Response get(String urlPath, String cookie);
-
-    public Response post(String apiParams, String urlPath, String cookie);
-
-    public void setUserAgent(String agent);
-}
