15cb5fd78db55dc42df85fe52616ba098e5ebf4c
ryan <vitineth@gmail.com>
2015-01-04 17:39:02 +0000

    Flair examples

--- src/main/java/examples/FlairExample.java
+++ src/main/java/examples/FlairExample.java
@@ -32,34 +32,40 @@ public class FlairExample {
         //Create the flair actions controlled
         FlairActions flairActions = new FlairActions(restClient, user);
 
-//        System.out.println(flairActions.flair("red", null, "Vitineth", "Test", "myblueprints").getStatusCode());
-//
-//        //Add the flair 'Blueprinter' to the post 't3_2r86db' in the subreddit 'myblueprints' with the css class 'red'
-//        System.out.println(flairActions.flair("red", "t3_2r86db", null, "Blueprinter", "myblueprints").getStatusCode());
-//
-//        //Add the flair 'Blueprinter' to the user 'Tridentac' in the subreddit 'myblueprints' with the css class 'red'
-//        System.out.println(flairActions.flair("red", null, "Tridentac", "Blueprinter", "myblueprints").getStatusCode());
-//
-//        //Delete the flair from the user 'Tridentac' in the subreddit 'myblueprints'
-//        System.out.println(flairActions.deleteFlair("Tridentac", "myblueprints").getStatusCode());
-//
-//        //Clear all user flair templates in the subreddit 'myblueprints'
-//        System.out.println(flairActions.clearFlairTemplates("USER_FLAIR", "myblueprints").getStatusCode());
-//
-//        //Clear all the link flair templates in the subreddit 'myblueprints'
-//        System.out.println(flairActions.clearFlairTemplates("LINK_FLAIR", "myblueprints").getStatusCode());
-//
-//        //Set the flair configs as so:
-//        //allow user and link flairs
-//        //user flairs on the left
-//        //link flairs on the right
-//        //users can assign link flairs
-//        //users cannot assign user flairs
-//        System.out.println(flairActions.flairConfig(true, "left", false, "right", true, "myblueprints").getStatusCode());
-//
+        System.out.println(flairActions.flair("red", null, "Vitineth", "Test", "myblueprints").getStatusCode());
 
-        System.out.println(flairActions.flairTemplate("red", "blueprinted-flair", "USER_FLAIR", "Blueprinted", false, "myblueprints"));
+        //Add the flair 'Blueprinter' to the post 't3_2r86db' in the subreddit 'myblueprints' with the css class 'red'
+        System.out.println(flairActions.flair("red", "t3_2r86db", null, "Blueprinter", "myblueprints").getStatusCode());
 
+        //Add the flair 'Blueprinter' to the user 'Tridentac' in the subreddit 'myblueprints' with the css class 'red'
+        System.out.println(flairActions.flair("red", null, "Tridentac", "Blueprinter", "myblueprints").getStatusCode());
+
+        //Delete the flair from the user 'Tridentac' in the subreddit 'myblueprints'
+        System.out.println(flairActions.deleteFlair("Tridentac", "myblueprints").getStatusCode());
+
+        //Clear all user flair templates in the subreddit 'myblueprints'
+        System.out.println(flairActions.clearFlairTemplates("USER_FLAIR", "myblueprints").getStatusCode());
+
+        //Clear all the link flair templates in the subreddit 'myblueprints'
+        System.out.println(flairActions.clearFlairTemplates("LINK_FLAIR", "myblueprints").getStatusCode());
+
+        //Set the flair configs as so:
+        //allow user and link flairs
+        //user flairs on the left
+        //link flairs on the right
+        //users can assign link flairs
+        //users cannot assign user flairs
+        System.out.println(flairActions.flairConfig(true, "left", false, "right", true, "myblueprints").getStatusCode());
+
+
+        //For example. Add the user flair template "BLUE-TAG" with the text "Blue Team" and the css class "blueteam" in the subreddit "/r/somegenericgame" with users not be able to edit the string and print the status code.
+        System.out.println(flairActions.flairTemplate("blueteam", "BLUE-TAG", "USER_FLAIR", "Blue Team", false, "somegenericgame").getStatusCode());
+
+        //For example. Give the user /u/somegenericgamer the flair "Blue team" from the flair-template "BLUE-TAG" in the subreddit /r/somegenericgame and print the status code.
+        System.out.println(flairActions.selectFlair("BLUE-TAG", null, "somegenericgamer", "Blue team", "somegenericgame").getStatusCode());
+
+        //For example. Enable flair for the currently logged in user in the subreddit /r/somegenericgame and print the status code.
+        System.out.println(flairActions.setFlairEnabled(true, "somegenericgame").getStatusCode());
     }
 
 
